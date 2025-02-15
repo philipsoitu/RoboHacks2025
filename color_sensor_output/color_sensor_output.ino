d0f448312d6c38daac6df1b06b416ca952319939
tran-ethan <tran.ethan@hotmail.com>
2025-02-14 17:36:56 -0800

    sensor out

--- color_sensor_output/color_sensor_output.ino
+++ color_sensor_output/color_sensor_output.ino
@@ -1,62 +1,104 @@
 // Define sensor pins
-#define S0 8         // D5
-#define S1 9         // D6
-#define S2 10        // D7
-#define S3 11        // D8
-#define sensorOut 12 // D9
-
-unsigned int redFreq = 0;
-unsigned int greenFreq = 0;
-unsigned int blueFreq = 0;
-
+#define S0 8          // D5
+#define S1 9          // D6
+#define S2 10         // D7
+#define S3 11         // D8
+#define out 12  // D9
+ 
+// Variables for Color Pulse Width Measurements
+ 
+int redPW = 0;
+int greenPW = 0;
+int bluePW = 0;
+ 
 void setup() {
-    // Set pin modes
-    pinMode(S0, OUTPUT);
-    pinMode(S1, OUTPUT);
-    pinMode(S2, OUTPUT);
-    pinMode(S3, OUTPUT);
-    pinMode(sensorOut, INPUT);
-
-    // Set frequency scaling to 20% (S0 HIGH, S1 LOW)
-    digitalWrite(S0, HIGH);
-    digitalWrite(S1, LOW);
-
-    Serial.begin(9600);
+ 
+  // Set S0 - S3 as outputs
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  // Set Sensor output as input
+  pinMode(out, INPUT);
+  
+  // Set Pulse Width scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+  // Setup Serial Monitor
+  Serial.begin(9600);
 }
-
+ 
 void loop() {
-    // Read RED frequency
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, LOW);
-    redFreq = pulseIn(sensorOut, LOW);
-
-    // Read GREEN frequency
-    digitalWrite(S2, HIGH);
-    digitalWrite(S3, HIGH);
-    greenFreq = pulseIn(sensorOut, LOW);
-
-    // Read BLUE frequency
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, HIGH);
-    blueFreq = pulseIn(sensorOut, LOW);
-
-    // Map frequencies to RGB values (inverse relationship)
-    int redValue = map(redFreq, 100, 3000, 255, 0);
-    int greenValue = map(greenFreq, 100, 3000, 255, 0);
-    int blueValue = map(blueFreq, 100, 3000, 255, 0);
-
-    // Ensure RGB values are within the 0-255 range
-    redValue = constrain(redValue, 0, 255);
-    greenValue = constrain(greenValue, 0, 255);
-    blueValue = constrain(blueValue, 0, 255);
-
-    // Print RGB values to Serial Monitor
-    Serial.print("R: ");
-    Serial.print(redValue);
-    Serial.print(" G: ");
-    Serial.print(greenValue);
-    Serial.print(" B: ");
-    Serial.println(blueValue);
-
-    delay(500);  // Wait before next reading
+  
+  // Read Red Pulse Width
+  redPW = getRedPW();
+  // Delay to stabilize sensor
+  delay(200);
+  
+  // Read Green Pulse Width
+  greenPW = getGreenPW();
+  // Delay to stabilize sensor
+  delay(200);
+  
+  // Read Blue Pulse Width
+  bluePW = getBluePW();
+  // Delay to stabilize sensor
+  delay(200);
+  
+  // Print output to Serial Monitor
+  Serial.print("Red PW = ");
+  Serial.print(redPW);
+  Serial.print(" - Green PW = ");
+  Serial.print(greenPW);
+  Serial.print(" - Blue PW = ");
+  Serial.println(bluePW);
+  
+}
+ 
+ 
+// Function to read Red Pulse Widths
+int getRedPW() {
+ 
+  // Set sensor to read Red only
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(out, LOW);
+  // Return the value
+  return PW;
+ 
+}
+ 
+// Function to read Green Pulse Widths
+int getGreenPW() {
+ 
+  // Set sensor to read Green only
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(out, LOW);
+  // Return the value
+  return PW;
+ 
+}
+ 
+// Function to read Blue Pulse Widths
+int getBluePW() {
+ 
+  // Set sensor to read Blue only
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(out, LOW);
+  // Return the value
+  return PW;
+ 
 }
