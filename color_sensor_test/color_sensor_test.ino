4b5d65047bd48dbbcfd045870810384a696844a9
tran-ethan <tran.ethan@hotmail.com>
2025-02-14 19:37:14 -0800

    sensor color converter working (not for blue)

--- color_sensor_test/color_sensor_test.ino
+++ color_sensor_test/color_sensor_test.ino
@@ -1,58 +1,66 @@
-/* This code works with GY-31 TCS3200 TCS230 color sensor module
- *  It select a photodiode set and read its value (Red Set/Blue set/Green set) and displays  it on the Serial monitor
- * Refer to www.surtrtech.com for more details
- */
+// TCS230 or TCS3200 pins wiring to Arduino
+#define S0 4
+#define S1 5
+#define S2 6
+#define S3 7
+#define sensorOut 8
 
-#define  s0 8     //d5
-#define s1 9      //d6
-#define s2 10     //d7
-#define s3 11     //d8
-#define  out 12   //d9
+// Stores frequency read by the photodiodes
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
 
-int data=0;        //This is where we're going to stock our values
-
-void  setup() 
-{
-   pinMode(s0,OUTPUT);    //pin modes
-   pinMode(s1,OUTPUT);
-   pinMode(s2,OUTPUT);
-   pinMode(s3,OUTPUT);
-   pinMode(out,INPUT);
-
-   Serial.begin(9600);   //intialize the serial monitor baud rate
-   
-   digitalWrite(s0,HIGH);  //Putting S0/S1 on HIGH/HIGH levels means the output frequency scalling is at 100%  (recommended)
-   digitalWrite(s1,HIGH); //LOW/LOW is off HIGH/LOW is 20% and  LOW/HIGH is  2%
-   
+void setup() {
+  // Setting the outputs
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  // Setting the sensorOut as an input
+  pinMode(sensorOut, INPUT);
+  
+  // Setting frequency scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+   // Begins serial communication 
+  Serial.begin(9600);
 }
-
-void loop()                  //Every 2s we select  a photodiodes set and read its data
-{
-
-   digitalWrite(s2,LOW);        //S2/S3  levels define which set of photodiodes we are using LOW/LOW is for RED LOW/HIGH  is for Blue and HIGH/HIGH is for green
-   digitalWrite(s3,LOW);
-   Serial.print("Red  value= "); 
-   GetData();                   //Executing GetData function to  get the value
-
-   digitalWrite(s2,LOW);
-   digitalWrite(s3,HIGH);
-   Serial.print("Blue  value= ");
-   GetData();
-
-   digitalWrite(s2,HIGH);
-   digitalWrite(s3,HIGH);
-   Serial.print("Green value= ");
-   GetData();
-
-   Serial.println();
-
-   delay(2000);
+void loop() {
+  // Setting RED (R) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  
+  // Reading the output frequency
+  redFrequency = pulseIn(sensorOut, LOW);
+  
+   // Printing the RED (R) value
+  Serial.print("R = ");
+  Serial.print(redFrequency);
+  delay(100);
+  
+  // Setting GREEN (G) filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  greenFrequency = pulseIn(sensorOut, LOW);
+  
+  // Printing the GREEN (G) value  
+  Serial.print(" G = ");
+  Serial.print(greenFrequency);
+  delay(100);
+ 
+  // Setting BLUE (B) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  blueFrequency = pulseIn(sensorOut, LOW);
+  
+  // Printing the BLUE (B) value 
+  Serial.print(" B = ");
+  Serial.println(blueFrequency);
+  delay(100);
 }
-
-void GetData(){
-   data=pulseIn(out,LOW);       //here  we wait until "out" go LOW, we start measuring the duration and stops when "out"  is HIGH again
-   Serial.print(data);          //it's a time duration measured,  which is related to frequency as the sensor gives a frequency depending on the color
-   Serial.print("\	");          //The higher the frequency the lower the duration
-   delay(20);
-}
-
